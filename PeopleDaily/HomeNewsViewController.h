8b660b20153d6b6e6e9c93984259bd16f3b93e14
YLRS413 <278051822@qq.com>
2014-05-14 23:33:13 +0800

    增加首页

---
+++ PeopleDaily/HomeNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeNewsViewController.h
+//  PeopleDaily
+//
+//  Created by YLRS on 5/14/14.
+//  Copyright (c) 2014 YLRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeNewsViewController : UIViewController
+
+@end
---
+++ PeopleDaily/HomeNewsViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeNewsViewController.m
+//  PeopleDaily
+//
+//  Created by YLRS on 5/14/14.
+//  Copyright (c) 2014 YLRS. All rights reserved.
+//
+
+#import "HomeNewsViewController.h"
+
+@interface HomeNewsViewController ()
+
+@end
+
+@implementation HomeNewsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
